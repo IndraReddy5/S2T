af4c030cf19c27f6ec895b648e379feb80c60991
IndraReddy5 <45683173+IndraReddy5@users.noreply.github.com>
2023-11-28 22:01:12 +0530

    added docker

---
+++ backend/deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Check if .env file exists
+if [ ! -f .env ]; then
+  echo "Error: .env file not found. Please create the .env file before running this script."
+  exit 1
+fi
+
+# Stop running containers
+echo "Stopping running containers ..."
+docker compose down
+
+# Delete old images
+echo "Delete old docker image ..."
+docker image rm S2T-api:latest
+
+# Checkout the `main` branch
+git checkout main
+
+# Pull the changes from main
+git pull
+
+# Deploy the current version
+echo "Building and deploying containers ..."
+docker compose up -d
