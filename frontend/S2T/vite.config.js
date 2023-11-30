1d48f665bdc60173c182adfa089b7c0fc28cb6ac
Indra Reddy <45683173+IndraReddy5@users.noreply.github.com>
2023-11-30 21:17:46 +0530

    Update vite.config.js

--- frontend/S2T/vite.config.js
+++ frontend/S2T/vite.config.js
@@ -1,20 +1,23 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  define: {
-    '__BACKEND_URL__' : JSON.stringify('http://localhost:8000/'),
-    '__File_URL__' : JSON.stringify('http://localhost:8000/static/')
-  },
+export default defineConfig( ({command, mode}) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [
+      vue(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    define: {
+      '__BACKEND_URL__' : JSON.stringify(env.BACKEND_URL),
+      '__File_URL__' : JSON.stringify(env.FILE_URL)
+    },
+  }
 })
